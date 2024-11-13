49bab2b5a66bb503269c4f5297b5b303230fd4b9
Jake Domermuth <146481362+jakedomermuth@users.noreply.github.com>
2024-11-13 17:01:32 -0500

    Update database.py

--- database.py
+++ database.py
@@ -63,6 +63,10 @@ SELECT_ASSISTANT = """SELECT * FROM assistants WHERE name = %s;"""
 
 SELECT_CLIENT = """SELECT * FROM clients WHERE client_name = %s;"""
 
+SELECT_CLIENT_BY_ID = """SELECT * FROM clients WHERE client_id = %s;"""
+
+SELECT_TAXES_BY_ID = """SELECT * FROM taxes WHERE tax_id = %s;"""
+
 # updating queries ----------------------------------------------------------------------------------------------------
 UPDATE_MATERIAL_STATUS = """UPDATE clients SET materials_provided = 'TRUE' WHERE client_id = %s; """
 
@@ -115,19 +119,46 @@ def add_taxes(connection, status, filed_timestamp, checked, client_id):
 
 
 # updating functions
+def update_material_status(connection, client_id: int):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(UPDATE_MATERIAL_STATUS, (client_id, ))
 
+
+def update_filing_status(connection, tax_id: int):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(UPDATE_FILED_STATUS, (tax_id, ))
+
+
+def update_checked_status(connection, tax_id: int):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(UPDATE_CHECKED_STATUS, (tax_id, ))
 
 
 #selecting functions
+def get_client(connection, client_id: int):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_CLIENT_BY_ID, (client_id, ))
+            client = cursor.fetchone()
+            return client
 
-def cpa_exists(connection, name):
+def get_taxes(connection, tax_id: int):
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_TAXES_BY_ID, (tax_id, ))
+            taxes = cursor.fetchone()
+            return taxes
+def cpa_exists(connection, name: str):
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(SELECT_CPA, (name,))
             cpa_name = cursor.fetchone()
             return cpa_name
 
-def assistant_exists(connection, name):
+def assistant_exists(connection, name: str):
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(SELECT_ASSISTANT, (name,))
@@ -135,7 +166,7 @@ def assistant_exists(connection, name):
             return assistant_name
 
 
-def client_exists(connection, name):
+def client_exists(connection, name: str):
     with connection:
         with connection.cursor() as cursor:
             cursor.execute(SELECT_CLIENT, (name,))
