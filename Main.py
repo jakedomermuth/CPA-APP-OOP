e0131b085328cdf11b622d10f0ea893da95ffc0e
Jake Domermuth <146481362+jakedomermuth@users.noreply.github.com>
2024-11-13 16:59:23 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -12,7 +12,7 @@ MENU_PROMPT = """
     3: Add Clients
     4: Update Provided Material Status
     5: Check Provided Material Status
-    6: Change filing status
+    6: Update filing status
     7: Check filing status
     8: Update CPA checked Return
     9: Check if CPA checked return
@@ -85,25 +85,25 @@ def new_tax_prompt():
     return status, current_timestamp, checked
 
 
-
 def update_client_prompt():
     input_id = input("What is the client's ID? ")
     client_id = Client.convert(input_id)
 
     client_instance = Client.get(client_id)
-    if client_instance.exists():
+    if client_instance is None:  # handing invalid id
+        print(f'A client with the ID {client_id} does not exist')
+    else:
         print(client_instance)
         update_choice = input('Has the client provided all necessary tax material?(True/False) ')
         choice = Client.upper(update_choice)
         if choice == 'TRUE':
             client_instance.update()
             print('Clients material status has been updated')
-            status, current_timestamp, checked = new_tax_prompt() # Once materials are provided, tax return info is immediatly taken
+            status, current_timestamp, checked = new_tax_prompt()  # Once materials are provided, tax return info is immediatly taken
 
             tax_instance = Taxes(status, current_timestamp, checked, client_instance.client_id)
-            tax_instance.save() # creating instance and saving tax info
-    else:
-        print(f'A client with the ID {client_id} does not exist')
+            tax_instance.save()  # creating instance and saving tax info
+
 
 
 def check_material_status():
@@ -111,8 +111,55 @@ def check_material_status():
     client_id = Client.convert(input_id)
 
     client_instance = Client.get(client_id)
-    print(client_instance)
+    if client_instance is None:  # handing invalid id
+        print(f'A client with the ID {client_id} does not exist')
+    else:
+        print(client_instance)
 
+
+def get_taxes():
+    input_id = input('What is the Tax ID you would like to check? ')
+    tax_id = Taxes.convert(input_id)
+
+    tax_instance = Taxes.get(tax_id)
+    if tax_instance is None:
+        print(f'A tax return with the id {tax_id} does not exist')
+    else:
+        print(tax_instance)
+
+
+def update_filing_status(): # change timestamp and get to update !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    input_id = input('What is the Tax ID you would like to check? ')
+    tax_id = Taxes.convert(input_id)
+
+    tax_instance = Taxes.get(tax_id)
+    if tax_instance is None:
+        print(f'A tax return with the id {tax_id} does not exist')
+    else:
+        print(tax_instance)
+        choice_input = input('Would you like to update the filing status to true?(True/False) ')
+        choice = Taxes.normalize(choice_input)
+        if choice == 'TRUE':
+            tax_instance.update() #NOT working, but no error
+            print('Clients filing status has been updated')
+
+def cpa_check_update(): # update doesnt work? !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    input_id = input('What is the Tax ID you would like to check? ')
+    tax_id = Taxes.convert(input_id)
+
+    tax_instance = Taxes.get(tax_id)
+    if tax_instance is None:
+        print(f'A tax return with the id {tax_id} does not exist')
+    else:
+        print(tax_instance)
+        if tax_instance.status == 'TRUE':
+            choice_input = input('Has the tax return been checked by a CPA?(True/False) ')
+            choice = Taxes.normalize(choice_input)
+            if choice == 'TRUE':
+                tax_instance.check() # NOT working but no error
+                print('Tax return has been updated to checked')
+        else:
+            print('This tax return has not been filed yet')
 
 MENU_OPTIONS = {
     '1': new_cpa_prompt,
@@ -120,10 +167,10 @@ MENU_OPTIONS = {
     '3': new_client_prompt,
     '4': update_client_prompt,
     '5': check_material_status,
-    '6': 'Change filing status',
-    '7': 'Check filing status',
-    '8': 'Update CPA checked Return',
-    '9': 'Check if CPA checked return'
+    '6': update_filing_status,
+    '7': get_taxes,
+    '8': cpa_check_update,
+    '9': get_taxes
 }
 
 def menu():
